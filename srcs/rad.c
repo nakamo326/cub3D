e2e4000c701b00fdd241d3d9972fc1f53ab1ad90
Yasufumi Nakamoto <nakamo326@gmail.com>
2020-11-29 10:52:12 +0900

    add some function

--- srcs/rad.c
+++ srcs/rad.c
@@ -1,26 +1,24 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   cub3D.h                                            :+:      :+:    :+:   */
+/*   rad.c                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ynakamot <ynakamot@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/11/28 11:01:45 by ynakamot          #+#    #+#             */
-/*   Updated: 2020/11/28 23:27:00 by ynakamot         ###   ########.fr       */
+/*   Created: 2020/11/28 23:20:57 by ynakamot          #+#    #+#             */
+/*   Updated: 2020/11/28 23:27:50 by ynakamot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef CUB3D_H
-# define CUB3D_H
+#include "../includes/cub3D.h"
+
+double degtorad(double deg)
+{
+	return (deg * (PI / 180.0));
+}
 
 
-#include <fcntl.h>
-#include <stdio.h>
-#include <string.h>
-#include <math.h>
-#include "../libft/libft.h"
-#include "../minilibx-linux/mlx.h"
-
-#define PI 3.14159
-
-#endif
+double radtodeg(double rad)
+{
+	return (rad * (180.0 / PI));
+}
