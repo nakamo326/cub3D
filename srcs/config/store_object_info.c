c273e6d8f8cfa69c3c296b88ada15384bd9469e0
Yasufumi Nakamoto <nakamo326@gmail.com>
2020-12-06 11:29:57 +0900

    change item's cordinate

--- srcs/config/store_object_info.c
+++ srcs/config/store_object_info.c
@@ -6,7 +6,7 @@
 /*   By: ynakamot <ynakamot@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/06 11:02:58 by ynakamot          #+#    #+#             */
-/*   Updated: 2020/12/06 11:14:55 by ynakamot         ###   ########.fr       */
+/*   Updated: 2020/12/06 11:27:33 by ynakamot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,8 @@ void	store_item_info(t_cub *cub, int x, int y)
 	t_sprite	*item_info;
 	static int	num = 0;
 	item_info = malloc(sizeof(t_sprite));
-	item_info->x = x;
-	item_info->y = y;
+	item_info->x = x * TILE_SIZE;
+	item_info->y = y * TILE_SIZE;
 	item_info->number = num++;
 	if (!(new = ft_lstnew(item_info)))
 		config_error(MALLOC_ERROR);
--- srcs/render/render_map_object.c
+++ srcs/render/render_map_object.c
@@ -6,7 +6,7 @@
 /*   By: ynakamot <ynakamot@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/05 12:18:39 by ynakamot          #+#    #+#             */
-/*   Updated: 2020/12/06 11:06:24 by ynakamot         ###   ########.fr       */
+/*   Updated: 2020/12/06 11:28:58 by ynakamot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,12 +32,12 @@ void	render_items(t_img *map, int x, int y)
 	int j;
 
 	i = 0;
-	x -= TILE_SIZE / 4;
-	y -= TILE_SIZE / 4;
-	while (i <= TILE_SIZE / 2)
+	x += TILE_SIZE * 3 / 8;
+	y += TILE_SIZE * 3 / 8;
+	while (i <= TILE_SIZE / 4)
 	{
 		j = 0;
-		while (j <= TILE_SIZE / 2)
+		while (j <= TILE_SIZE / 4)
 		{
 			my_mlx_pixel_put(map, x + i, y + j, 0x2DC8FF);
 			j++;
@@ -50,19 +50,13 @@ void	render_map_object(t_game *game)
 {
 	t_list		*lstptr;
 	t_sprite	*item_info;
-	int			x;
-	int			y;
-	//change after define map_tilesize
-	render_player(&game->map, game->player.x, game->player.y);
 
+	render_player(&game->map, game->player.x, game->player.y);
 	lstptr = game->cub.items;
 	while (lstptr != NULL)
 	{
 		item_info = lstptr->content;
-		x = item_info->x;
-		y = item_info->y;
-		render_items(&game->map, x * TILE_SIZE + TILE_SIZE /2,
-			y * TILE_SIZE + TILE_SIZE /2);
+		render_items(&game->map, item_info->x, item_info->y);
 		lstptr = lstptr->next;
 	}
 
