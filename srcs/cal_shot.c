e1d553b21e7146bd425f8c211a163ba5ff14a33f
Yasufumi Nakamoto <nakamo326@gmail.com>
2021-01-15 12:28:55 +0900

    fix norm error

--- srcs/cal_shot.c
+++ srcs/cal_shot.c
@@ -6,11 +6,33 @@
 /*   By: ynakamot <ynakamot@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/14 22:21:15 by ynakamot          #+#    #+#             */
-/*   Updated: 2021/01/15 12:20:41 by ynakamot         ###   ########.fr       */
+/*   Updated: 2021/01/15 12:28:39 by ynakamot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
+
+void	update_sp_life(t_game *game, int num)
+{
+	int			i;
+	t_list		*items;
+	t_sprite	*item_info;
+
+	items = game->cub.items;
+	i = 0;
+	while (items != NULL)
+	{
+		if (i == num)
+		{
+			item_info = (t_sprite *)items->content;
+			if (item_info->life > 0)
+				item_info->life -= 1;
+			break ;
+		}
+		items = items->next;
+		i++;
+	}
+}
 
 void	cal_shot(t_game *game)
 {
@@ -31,20 +53,7 @@ void	cal_shot(t_game *game)
 		items = items->next;
 		i++;
 	}
-	items = game->cub.items;
-	i = 0;
-	while (items != NULL)
-	{
-		if (i == num)
-		{
-			item_info = (t_sprite *)items->content;
-			if (item_info->life > 0)
-				item_info->life -= 1;
-			break ;
-		}
-		items = items->next;
-		i++;
-	}
+	update_sp_life(game, num);
 	game->shot = false;
 	return ;
 }
