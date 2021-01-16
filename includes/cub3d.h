70411bb51c152393d4a863b78586dbc20d53b9ad
Yasufumi Nakamoto <nakamo326@gmail.com>
2021-01-16 18:08:21 +0900

    add minilibX

--- includes/cub3d.h
+++ includes/cub3d.h
@@ -6,13 +6,12 @@
 /*   By: ynakamot <ynakamot@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/28 11:01:45 by ynakamot          #+#    #+#             */
-/*   Updated: 2021/01/16 13:36:26 by ynakamot         ###   ########.fr       */
+/*   Updated: 2021/01/16 18:08:04 by ynakamot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CUB3D_H
 # define CUB3D_H
-
 
 #include <fcntl.h>
 #include <sys/types.h>
